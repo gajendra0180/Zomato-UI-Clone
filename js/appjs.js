1702f9f79856a8f3d4a33caec02971a5581064e7
Gajendra <gajendrahitz@gmail.com>
2021-04-19 19:52:20 +0530

    add to cart

--- js/appjs.js
+++ js/appjs.js
@@ -1,7 +1,13 @@
-var itemcount = 0;
+var itemcount = 1;
 
 function addtocart() {
-    alert("Item Added to Cart");
+    alert("𝓒𝓞𝓝𝓖𝓞 🎉 𝓘𝓣𝓔𝓜 𝓐𝓓𝓓𝓔𝓓 𝓣𝓞 𝓒𝓐𝓡𝓣 😂");
     document.getElementById("itemcount").innerHTML = itemcount;
     itemcount++;
+}
+
+function gotocart() {
+    document.getElementById("cartdisplay").innerHTML = "";
+    document.getElementById("cart").innerHTML = "";
+
 }
