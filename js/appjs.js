c813dc4d3be7c1fca3b1e56839fad339c1b0623f
Gajendra <gajendrahitz@gmail.com>
2021-04-19 21:26:50 +0530

    made order cart

--- js/appjs.js
+++ js/appjs.js
@@ -1,13 +1,51 @@
 var itemcount = 1;
+var arr = []
 
-function addtocart() {
+function addtocart(ids) {
     alert("𝓒𝓞𝓝𝓖𝓞 🎉 𝓘𝓣𝓔𝓜 𝓐𝓓𝓓𝓔𝓓 𝓣𝓞 𝓒𝓐𝓡𝓣 😂");
+    arr.push(ids);
     document.getElementById("itemcount").innerHTML = itemcount;
     itemcount++;
 }
 
 function gotocart() {
-    document.getElementById("cartdisplay").innerHTML = "";
     document.getElementById("cart").innerHTML = "";
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("card13").style.display = "none";
+    document.getElementById("card14").style.display = "none";
+    document.getElementById("card15").style.display = "none";
+    document.getElementById("inspiration-order-box").innerHTML = "<div><hr/></div>";
+    document.getElementById("togglecart").innerHTML = "";
+    // document.getElementById("best-food").style.fontSize = "50%";
+    document.getElementById("best-food").marginBottom = "15vh"
+
+    document.getElementById("best-food").style.fontSize = "50px";
+    document.getElementById("best-food").innerHTML = "ITEMS IN THE CART"
+
+
+
+
+
+
+    document.getElementById("totalamount").innerHTML = ' <div class="popular-restaurants-text">Total Amount : </div>'
+
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+        document.getElementById(arr[i]).style.display = "flex";
+
+    }
 
 }
